b54fdd9eb30e16cee4fb5e00085c8b4d67b7e23a
Darko <dpranjic@iolap.com>
2018-11-07 13:02:51 +0100

    Removed unecessary resolver

--- server/src/resolvers/index.js
+++ server/src/resolvers/index.js
@@ -1,14 +1,7 @@
 const { Query } = require('./Query');
 const { Mutation } = require('./Mutation');
 
-const AuthPayload = {
-	user: async ({ user: { id } }, args, ctx, info) => {
-		return ctx.db.query.user({ where: { id } }, info);
-	},
-};
-
 const resolvers = {
-	AuthPayload,
 	Mutation,
 	Query,
 };
