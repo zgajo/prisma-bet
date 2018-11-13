b4780d7ca16aff8159daf5bffec7ff7521a5c956
Darko <dpranjic@iolap.com>
2018-11-13 18:04:26 +0100

    Add nodemailer on signup

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,5 +1,11 @@
+/**
+ * TODO:
+ * Remove eslint comment
+ */
+// eslint-disable-next-line
 const { hash, compare } = require('bcrypt');
 const { sign } = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 const { APP_SECRET } = require('../utils');
 
 const Mutation = {
@@ -23,21 +29,62 @@ const Mutation = {
 			user,
 		};
 	},
+	/**
+	 * TODO:
+	 * Remove eslint comment
+	 */
+	// eslint-disable-next-line
 	signup: async (_, { name, username, password }, ctx) => {
-		const hashedPassword = await hash(password, 10);
+		try {
+			/**
+			 * TODO:
+			 * Set this mail sending into utils folder
+			 */
 
-		const user = await ctx.db.mutation.createUser({
-			data: {
-				name,
-				password: hashedPassword,
-				username,
-			},
-		});
+			// TODO: Logic on confirming user creation in mail
+			let transport = nodemailer.createTransport({
+				auth: {
+					pass: process.env.OUTLOOK_PASSWORD,
+					user: process.env.OUTLOOK_MAIL,
+				},
+				host: 'smtp-mail.outlook.com', // hostname
+				port: 587, // port for secure SMTP
+				secureConnection: false, // TLS requires secureConnection to be false
+				tls: {
+					ciphers: 'SSLv3',
+				},
+			});
 
-		return {
-			token: sign({ userId: user.id }, APP_SECRET),
-			user,
-		};
+			let message = {
+				from: process.env.OUTLOOK_MAIL,
+				html: '<p>HTML version of the message</p>',
+				subject: 'Creating new user',
+				// TODO: Add user info in body text
+				text: 'Plain text version of the message',
+				to: process.env.MAIN_EMAIL,
+			};
+
+			await transport.sendMail(message);
+
+			return true;
+		} catch (error) {
+			return false;
+		}
+
+		// const hashedPassword = await hash(password, 10);
+
+		// const user = await ctx.db.mutation.createUser({
+		// 	data: {
+		// 		name,
+		// 		password: hashedPassword,
+		// 		username,
+		// 	},
+		// });
+
+		// return {
+		// 	token: sign({ userId: user.id }, APP_SECRET),
+		// 	user,
+		// };
 	},
 };
 
